8e2704417e339eedd2bef1c25446a34e3ec07d4a
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-07-20 11:26:28 -0400

    The Village at Orange 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "The District at Green Valley Ranch",
                     "default_image" : "http://assets.codecloudapp.com/sites/575884676e6f640940010000/image/jpeg/undefined/tvo_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "196558634"
+                    "social_feed" : "http://villageatorange.mallmaverick.com/api/v2/villageorange/social.json"
                 }
