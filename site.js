b548914b51a69be94f55fbb132213a272936ccc6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 11:49:08 -0400

    The Village at Orange 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "The District at Green Valley Ranch",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f3cba56e6f64554f020000/image/jpeg/undefined/tvo_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//villageatorange.mallmaverick.com/api/v2/villageorange/social.json"
+                }
